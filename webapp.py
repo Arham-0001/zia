2afba85ccd075ab48e4d2a86042e2f87251e62f0
Arham <arhamrehamn278@gmail.com>
2025-11-07 21:31:33 +0530

    add the new feature and improve the voice input

--- webapp.py
+++ webapp.py
@@ -2,5 +2,8 @@ app={
     "youtube":"https://youtube.com",
     "spotify":"https://open.spotify.com",
     "chatgpt":"https://chat.openai.com",
-    "email":"https://mail.google.com"
+    "email":"https://mail.google.com",
+    "instagram":"https://instagram.com",
+    "snapchat": "https://snapchat.com",
+
 }
